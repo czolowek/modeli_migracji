1826cdc0a5029aae8e565b0b2836c88d13c0bba6
czolowek <klutoi39@gmail.com>
2025-11-23 18:40:21 +0100

    аы
    ыва

--- myproject/library_app/models.py
+++ myproject/library_app/models.py
@@ -8,25 +8,30 @@ class StudentGroup(models.Model):
     def __str__(self):
         return f"{self.group_number}"
 
+
 class LibraryCard(models.Model):
     card_id = models.CharField(max_length=50)
     issue_date = models.DateField()
     expire_date = models.DateField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
+    
     def __str__(self):
         return self.card_id
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     student_id = models.CharField(max_length=50)
     email = models.EmailField()
+
     group = models.ForeignKey(StudentGroup, on_delete=models.SET_NULL, null=True, blank=True)
+
     library_card = models.ForeignKey(LibraryCard, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
 
 class Literature(models.Model):
     lit_id = models.CharField(max_length=50)
@@ -38,6 +43,7 @@ class Literature(models.Model):
     def __str__(self):
         return self.title
 
+
 class TakeBookProcess(models.Model):
     card_id = models.CharField(max_length=50)
     literature_id = models.CharField(max_length=50)
@@ -46,3 +52,55 @@ class TakeBookProcess(models.Model):
 
     def __str__(self):
         return f"{self.card_id} -> {self.literature_id}"
+
+
+
+class Passport(models.Model):
+    passport_number = models.CharField(max_length=50)
+    issued_by = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.passport_number
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+    passport = models.OneToOneField(Passport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
